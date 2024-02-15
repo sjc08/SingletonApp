eec64128397057c1d335e8ccf317cd916c3e8162
AlexSJC <cn_sjc@outlook.com>
2024-02-15 22:06:04 +0800

    添加项目文件。

---
+++ README.md
@@ -0,0 +1,2 @@
+This library allows you to create single instance applications.
+
---
+++ SingletonApp/SingletonApp.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+
+namespace AS
+{
+    namespace SingletonApp
+    {
+        public enum MutexScope
+        {
+            Local,
+            Global
+        }
+
+        public static class SingletonApp
+        {
+            private static Mutex? mutex;
+
+            public static bool IsNew { get; private set; }
+
+            public static bool Check()
+            {
+                return Check(MutexScope.Local);
+            }
+
+            public static bool Check(MutexScope scope)
+            {
+                return Check($"{scope}\\{Assembly.GetEntryAssembly()?.GetName().Name}");
+            }
+
+            public static bool Check(string name)
+            {
+                bool createdNew;
+                mutex = new(true, name, out createdNew);
+                IsNew = createdNew;
+                return createdNew;
+            }
+
+            public static void Cleanup()
+            {
+                mutex?.Close();
+            }
+        }
+    }
+}
