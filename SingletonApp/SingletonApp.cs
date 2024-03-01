c51334f3d85e85f5987e35c97324b0d128bc508e
AlexSJC <cn_sjc@outlook.com>
2024-03-01 22:55:08 +0800

    Fix the namespace.

--- SingletonApp/SingletonApp.cs
+++ SingletonApp/SingletonApp.cs
@@ -1,43 +1,40 @@
 ﻿using System.Reflection;
 
-namespace AS
+namespace Asjc.SingletonApp
 {
-    namespace SingletonApp
+    public enum MutexScope
     {
-        public enum MutexScope
+        Local,
+        Global
+    }
+
+    public static class SingletonApp
+    {
+        private static Mutex? mutex;
+
+        public static bool IsNew { get; private set; }
+
+        public static bool Check()
         {
-            Local,
-            Global
+            return Check(MutexScope.Local);
         }
 
-        public static class SingletonApp
+        public static bool Check(MutexScope scope)
         {
-            private static Mutex? mutex;
+            return Check($"{scope}\\{Assembly.GetEntryAssembly()?.GetName().Name}");
+        }
 
-            public static bool IsNew { get; private set; }
+        public static bool Check(string name)
+        {
+            bool createdNew;
+            mutex = new(true, name, out createdNew);
+            IsNew = createdNew;
+            return createdNew;
+        }
 
-            public static bool Check()
-            {
-                return Check(MutexScope.Local);
-            }
-
-            public static bool Check(MutexScope scope)
-            {
-                return Check($"{scope}\\{Assembly.GetEntryAssembly()?.GetName().Name}");
-            }
-
-            public static bool Check(string name)
-            {
-                bool createdNew;
-                mutex = new(true, name, out createdNew);
-                IsNew = createdNew;
-                return createdNew;
-            }
-
-            public static void Cleanup()
-            {
-                mutex?.Close();
-            }
+        public static void Cleanup()
+        {
+            mutex?.Close();
         }
     }
 }
